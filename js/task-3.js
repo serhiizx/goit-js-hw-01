889c32be5f2e5bc9d1b837dfca75a189c3bb4fb5
Serhii Zhdaniuk <jdanuk@gmail.com>
2026-04-01 13:53:50 +0300

    fix: task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function getElementWidth(content, padding, border) {
-  return parseInt(content) + parseInt(padding) * 2 + parseFloat(border) * 2;
+  return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
 }
 
 console.log(getElementWidth('50px', '8px', '4px')); // 74
